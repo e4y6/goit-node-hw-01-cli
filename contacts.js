6d9fe75fdb52d5005d3d149b77d13124fe201d8a
e4y6 <99596168+e4y6@users.noreply.github.com>
2022-07-18 16:29:23 +0300

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,42 @@
+const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function updateContacts(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const oneContact = contacts.find((item) => item.id === contactId);
+  return oneContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [contactToRemove] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return contactToRemove;
+}
+
+async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = { name, email, phone, id: nanoid() };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
